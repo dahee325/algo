f6d8731b8f0c7ae18c1f2ae554760e7ef580b22e
changhee <oche.edu@gmail.com>
2025-02-14 16:06:12 +0900

    algo

---
+++ 짝수의합/sol.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+
+    for i in range(n+1):
+        if i % 2 == 0:
+            answer = answer + i
+            # answer += i
+
+    return answer
+
+print(solution(10))
+print(solution(4))
