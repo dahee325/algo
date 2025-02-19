dae3d6c3e7e859bc938a831fcf1b8dcd8cf9ec23
dahee <sally8323@gmail.com>
2025-02-19 17:20:40 +0900

    update

---
+++ 피자나눠먹기3/sol.py
@@ -0,0 +1,11 @@
+def solution(slice, n):
+    answer = 0
+    if n % slice == 0:
+        answer = n // slice
+    else:
+        answer = n // slice + 1
+    return answer
+
+
+print(solution(7, 10))
+print(solution(4, 12))
