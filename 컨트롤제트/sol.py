1a0ab2c02ce6d9d9f8c5ff5ca152bae70a487d67
dahee325 <sally8323@gmail.com>
2025-04-12 19:41:01 +0900

    250412

--- 컨트롤제트/sol.py
+++ 컨트롤제트/sol.py
@@ -1,10 +1,12 @@
 def solution(s):
-    answer = 0
+    answer = []
     s = s.split(' ')
-    for number in range(0, len(s)):
-        if s[number] == 'Z':
-            s.remove(s[number-1])
-    return s
+    for string in s:
+        if string != 'Z':
+            answer.append(int(string))
+        else:
+            answer.pop()
+    return sum(answer)
 
 
 print(solution('1 2 Z 3')) # => 4
